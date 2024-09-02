6bed274356e01941f3434ead73c5b5582f27f697
Yosef Siebzener <zibz0027@gmail.com>
2024-09-01 20:54:41 -0400

    initial commit

---
+++ ConsoleApp1/Classes/DayAndSlotNumber.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1.Classes
+{
+    public class DayAndSlotNumber
+    {
+        public int Day { get; set; }
+        public int SlotNumber { get; set; }
+
+        public DayAndSlotNumber(int day, int slotNumber)
+        {
+            Day = day;
+            SlotNumber = slotNumber;
+        }
+    }
+}
